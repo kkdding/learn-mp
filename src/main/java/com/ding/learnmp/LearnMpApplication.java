10c4a2f59f362132e4fb35b1e230661db06fdda6
wmding <2386585789@qq.com>
2024-08-11 12:17:03 +0800

    init

---
+++ src/main/java/com/ding/learnmp/LearnMpApplication.java
@@ -0,0 +1,13 @@
+package com.ding.learnmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnMpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ding/learnmp/LearnMpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ding.learnmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
